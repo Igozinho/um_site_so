2695200602f2cf98f2b0fa64cbdb04b686bc14f8
Igor mateus <Igormateus17rios@gmail.com>
2024-11-07 11:49:28 -0300

    0.1

--- python 3.py
+++ python 3.py
@@ -1,3 +1,8 @@
-class PilhaEncadeada:
-    	def __init__(self,topo=None):
-        		self.topo = topo
+m = [6,96,3,56,78,956,0]
+i=9;
+x=7;
+if i==9:
+    resultado = i * x
+    print(resultado);
+    print("o resulta do de 9 * 7 é: ",i*x);
+print(m)
